ecdd862f6fcc7785015163b2f5ef65bc474fc120
rainbow <sendtorainbow-d@yahoo.com.cn>
2011-08-19 11:08:23 +0800

    从Init中分离配置文件读取

--- qqstatus-core/src/main/java/cn/com/lazyhome/qqstatus/util/Init.java
+++ qqstatus-core/src/main/java/cn/com/lazyhome/qqstatus/util/Init.java
@@ -27,78 +27,29 @@ public class Init implements ServletContextListener {
 	 */
 	public void contextInitialized(ServletContextEvent event) {
 		rootpath = event.getServletContext().getRealPath("/");
-		//间隔1天，邮件通知任务
-		long period;
-		// 延迟一定时间执行
-		long delay;
-		// 检查在线状态间隔
-		long fetch_period;
-		// 统计信息图片大小
-		int width, height;
-		String url_begin;
-		String url_end;
-		long imageSize;
-		
-		Properties props = new Properties();
-		try {
-			// 从配置文件读取参数
-			String file = rootpath + "/WEB-INF/classes/qqstatus.properties";
-			logger.debug("qqstatus.properties path:\t" + file);
-			FileInputStream fis = new FileInputStream(file);
-			props.load(fis);
-			String prop = props.getProperty("mail.notify.period");
-			period = Long.parseLong(prop);
-			
-			prop = props.getProperty("mail.notify.delay");
-			delay = Long.parseLong(prop);
-			
-			prop = props.getProperty("fetcher.period");
-			fetch_period = Long.parseLong(prop);
-			
-			prop = props.getProperty("image.height");
-			height = Integer.parseInt(prop);
 
-			prop = props.getProperty("image.width");
-			width = Integer.parseInt(prop);
-			
-			// URL
-			url_begin = props.getProperty("image.url.begin");
-			url_end = props.getProperty("image.url.end");
-			
-			prop = props.getProperty("image.size");
-			imageSize = Integer.parseInt(prop);
-			
-		} catch (Exception e) {
-			period = 1000 * 60 * 60 * 24;
-			delay = 5000;
-			fetch_period = 60000;
-			
-			
-			width = 200;
-			height = 15000;
-			url_begin = "http://wpa.qq.com/pa?p=2:";
-			url_end = ":41";
-			imageSize = 1243;
-			
-			logger.warn("failed load qqstatus.properties file!! use the default config.");
-			logger.warn(e.getMessage(), e);
-		}
+		String file = rootpath + "/WEB-INF/classes/qqstatus.properties";
+		SysConfig conf = new SysConfig();
+		conf.load(file);
 
-		LineChart.WIDTH = width;
-		LineChart.HEIGHT = height;
-		CheckStatus.URL_BEGIN = url_begin;
-		CheckStatus.URL_END = url_end;
-		CheckStatus.IMAGE_SIZE = imageSize;
+		LineChart.WIDTH = conf.getImageWidth();
+		LineChart.HEIGHT = conf.getImageHeight();
+		CheckStatus.URL_BEGIN = conf.getImageUrlBegin();
+		CheckStatus.URL_END = conf.getImageUrlEnd();
+		CheckStatus.IMAGE_SIZE = conf.getImageSize();
 		
 		// 定时查询状态
 		fetcher = new FetchStatus();
-		fetcher.setPeriod(fetch_period);
+		fetcher.setRun(conf.isRunFetcher());
+		fetcher.setPeriod(conf.getFetcherPeriod());
 		
 		thread = new Thread(fetcher);
 		thread.start();
 		
 		// 上线邮件通知
 		tracker = new Tracker();
+		tracker.setRun(conf.isRunTracker());
+		tracker.setPeriod(conf.getTrackerPeriod());
 		new Thread(tracker).start();
 		
 		// 每日历史状态通知
@@ -117,7 +68,7 @@ public class Init implements ServletContextListener {
 			firstTime.add(Calendar.DAY_OF_YEAR, 1);
 		}
 		
-		timer.schedule(task, firstTime.getTime(), period);
+		timer.schedule(task, firstTime.getTime(), conf.getMailPeriod());
 	}
 
 	/**
---
+++ qqstatus-core/src/main/java/cn/com/lazyhome/qqstatus/util/SysConfig.java
@@ -0,0 +1,181 @@
+package cn.com.lazyhome.qqstatus.util;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class SysConfig {
+	private static Log logger = LogFactory.getLog(SysConfig.class);
+	
+	// 间隔1天，邮件通知任务
+	private long mailPeriod;
+	// 采集器启动开关
+	private boolean runFetcher, runTracker;
+	// 延迟一定时间执行
+	private long fetcherdelay;
+	// 检查在线状态间隔
+	private long fetcherPeriod;
+	private long trackerPeriod;
+	// image统计信息图片大小
+	private int imageWidth, imageHeight;
+	private String imageUrlBegin;
+	private String imageUrlEnd;
+	private long imageSize;
+	
+	public void load(String filepath) {
+
+		Properties props = new Properties();
+		try {
+			// 从配置文件读取参数
+			String file = filepath;
+			logger.debug("qqstatus.properties path:\t" + file);
+			FileInputStream fis = new FileInputStream(file);
+			props.load(fis);
+			String prop;
+			
+			prop = props.getProperty("fetcher.period");
+			fetcherPeriod = Long.parseLong(prop);
+			
+			prop = props.getProperty("fetcher.run");
+			runFetcher = prop.equals("true");
+			
+			//image
+			prop = props.getProperty("image.height");
+			imageHeight = Integer.parseInt(prop);
+
+			prop = props.getProperty("image.width");
+			imageWidth = Integer.parseInt(prop);
+			
+			// URL
+			imageUrlBegin = props.getProperty("image.url.begin");
+			imageUrlEnd = props.getProperty("image.url.end");
+			
+			prop = props.getProperty("image.size");
+			imageSize = Integer.parseInt(prop);
+			
+			// tracker
+			prop = props.getProperty("tracker.run");
+			runTracker = prop.equals("true");
+			prop = props.getProperty("tracker.period");
+			trackerPeriod = Long.parseLong(prop);
+			
+			//mail
+			prop = props.getProperty("mail.notify.period");
+			mailPeriod = Long.parseLong(prop);
+			
+			prop = props.getProperty("mail.notify.delay");
+			fetcherdelay = Long.parseLong(prop);
+			
+		} catch (Exception e) {
+			runFetcher = false;
+			mailPeriod = 1000 * 60 * 60 * 24;
+			fetcherdelay = 5000;
+			fetcherPeriod = 60000;
+			
+			runTracker = false;
+			trackerPeriod = 60000;
+			
+			
+			imageWidth = 200;
+			imageHeight = 15000;
+			imageUrlBegin = "http://wpa.qq.com/pa?p=2:";
+			imageUrlEnd = ":41";
+			imageSize = 1243;
+			
+			logger.warn("failed load qqstatus.properties file!! use the default config.");
+			logger.warn(e.getMessage(), e);
+		}
+	}
+
+	public long getMailPeriod() {
+		return mailPeriod;
+	}
+
+	public void setMailPeriod(long period) {
+		this.mailPeriod = period;
+	}
+
+	public boolean isRunFetcher() {
+		return runFetcher;
+	}
+
+	public void setRunFetcher(boolean runFetcher) {
+		this.runFetcher = runFetcher;
+	}
+
+	public boolean isRunTracker() {
+		return runTracker;
+	}
+
+	public void setRunTracker(boolean runTracker) {
+		this.runTracker = runTracker;
+	}
+
+	public long getFetcherdelay() {
+		return fetcherdelay;
+	}
+
+	public void setFetcherdelay(long fetcherdelay) {
+		this.fetcherdelay = fetcherdelay;
+	}
+
+	public long getFetcherPeriod() {
+		return fetcherPeriod;
+	}
+
+	public void setFetcherPeriod(long fetcherPeriod) {
+		this.fetcherPeriod = fetcherPeriod;
+	}
+
+	public long getTrackerPeriod() {
+		return trackerPeriod;
+	}
+
+	public void setTrackerPeriod(long trackerPeriod) {
+		this.trackerPeriod = trackerPeriod;
+	}
+
+	public int getImageWidth() {
+		return imageWidth;
+	}
+
+	public void setImageWidth(int imageWidth) {
+		this.imageWidth = imageWidth;
+	}
+
+	public int getImageHeight() {
+		return imageHeight;
+	}
+
+	public void setImageHeight(int imageHeight) {
+		this.imageHeight = imageHeight;
+	}
+
+	public String getImageUrlBegin() {
+		return imageUrlBegin;
+	}
+
+	public void setImageUrlBegin(String imageUrlBegin) {
+		this.imageUrlBegin = imageUrlBegin;
+	}
+
+	public String getImageUrlEnd() {
+		return imageUrlEnd;
+	}
+
+	public void setImageUrlEnd(String imageUrlEnd) {
+		this.imageUrlEnd = imageUrlEnd;
+	}
+
+	public long getImageSize() {
+		return imageSize;
+	}
+
+	public void setImageSize(long imageSize) {
+		this.imageSize = imageSize;
+	}
+	
+	
+}
