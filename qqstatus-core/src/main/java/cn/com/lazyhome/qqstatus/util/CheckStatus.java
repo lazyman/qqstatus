e2a8501314b669b1a32ad230a9fead3513e00677
rainbow <sendtorainbow-d@yahoo.com.cn>
2013-03-22 23:02:05 +0800

    增加public Log checking(String qqno) 的throws Exception 当出现异常时直接抛出Exception，不再返回null

--- qqstatus-core/src/main/java/cn/com/lazyhome/qqstatus/util/CheckStatus.java
+++ qqstatus-core/src/main/java/cn/com/lazyhome/qqstatus/util/CheckStatus.java
@@ -30,50 +30,44 @@ public class CheckStatus {
 
 	/**
 	 * 根据QQ号查完状态返回封装好的记录
+	 * @throws IOException 
 	 */
-	public Log checking(String qqno) {
+	public Log checking(String qqno) throws IOException {
 		String url_s = URL_BEGIN + qqno + URL_END;
 
-		try {
-			URL url = new URL(url_s);
-			// 打开连接
-			HttpURLConnection httpConnection = (HttpURLConnection) url
-					.openConnection();
+		URL url = new URL(url_s);
+		// 打开连接
+		HttpURLConnection httpConnection = (HttpURLConnection) url
+				.openConnection();
 
-			InputStream input = httpConnection.getInputStream();
-			byte[] b = new byte[1024];
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			// 读取网络文件,写入指定的文件中
-			int nRead;
-			while ((nRead = input.read(b)) > 0 ) {
-				baos.write(b, 0, nRead);
-			}
-			httpConnection.disconnect();
-			
-
-			// 获得文件长度
-			long nEndPos =getFileSize(url_s);
-			logger.debug("end pos:" + String.valueOf(nEndPos));
-			int status = 0;
-			status = IMAGE_SIZE == nEndPos?1:0;
-			logger.debug("status:" + String .valueOf(status));
-			
-			Blob file = new BlobImpl(baos.toByteArray());
-			Log log = new Log();
-			log.setQqId(qqno);
-			log.setTime(new Date());
-			log.setStatus(status);
-			log.setFileSize((int)nEndPos);
-			log.setFile(file);
-			
-			
-			return log;
-		} catch (IOException e) {
-			logger.fatal(e.getMessage(), e);
-			
-			return null;
+		InputStream input = httpConnection.getInputStream();
+		byte[] b = new byte[1024];
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		// 读取网络文件,写入指定的文件中
+		int nRead;
+		while ((nRead = input.read(b)) > 0 ) {
+			baos.write(b, 0, nRead);
 		}
+		httpConnection.disconnect();
+		
 
+		// 获得文件长度
+		long nEndPos =getFileSize(url_s);
+		logger.debug("end pos:" + String.valueOf(nEndPos));
+		int status = 0;
+		status = IMAGE_SIZE == nEndPos?1:0;
+		logger.debug("status:" + String .valueOf(status));
+		
+		Blob file = new BlobImpl(baos.toByteArray());
+		Log log = new Log();
+		log.setQqId(qqno);
+		log.setTime(new Date());
+		log.setStatus(status);
+		log.setFileSize((int)nEndPos);
+		log.setFile(file);
+		
+		
+		return log;
 	}
 
 	// 获得文件长度
@@ -114,8 +108,12 @@ public class CheckStatus {
 		String qqno = "109719189";
 		
 		CheckStatus check = new CheckStatus();
-		check.checking(qqno);
-		qqno = "84074663";
-		check.checking(qqno);
+		try {
+			check.checking(qqno);
+			qqno = "84074663";
+			check.checking(qqno);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
